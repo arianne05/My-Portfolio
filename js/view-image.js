78f1a6745895f37237396f68157071b9d8fd85e6
arianne05 <quimpoarianne5@gmail.com>
2023-10-09 19:05:56 +0800

    Closed modal onclicked outside the content

--- js/view-image.js
+++ js/view-image.js
@@ -1,13 +1,24 @@
-  /* Show modal */
-  function showImageModal(img) {
-    var modal = document.getElementById("imageModal");
-    var modalImg = document.getElementById("fullImage");
-    modal.style.display = "block";
-    modalImg.src = img.src;
+/* Show modal */
+function showImageModal(img) {
+  var modal = document.getElementById("imageModal");
+  var modalImg = document.getElementById("fullImage");
+  modal.style.display = "block";
+  modalImg.src = img.src;
+
+  // Add a click event listener to the modal overlay
+  modal.addEventListener("click", function(event) {
+      if (event.target === modal) {
+          closeImageModal(); // Close the modal if the click occurred outside the content
+      }
+  });
 }
 
 /* Close modal */
 function closeImageModal() {
-    var modal = document.getElementById("imageModal");
-    modal.style.display = "none";
+  var modal = document.getElementById("imageModal");
+  modal.style.display = "none";
 }
+
+// Close the modal when the close button is clicked
+var closeButton = document.querySelector(".closeImage");
+closeButton.addEventListener("click", closeImageModal);
