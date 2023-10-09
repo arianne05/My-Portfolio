10cc2f8f9b37fbb0e4cdbe04bc54d5ec6b4db98e
arianne05 <quimpoarianne5@gmail.com>
2023-10-09 17:51:08 +0800

    header-img is now changeable based on selected tab

--- js/tab-project.js
+++ js/tab-project.js
@@ -1,16 +1,28 @@
 const imgPrevCons = document.querySelectorAll('.img-prev-con');
 const textSecs = document.querySelectorAll('.tab-project-route');
+const headerImgs = document.querySelectorAll('.header-img');
 
 imgPrevCons.forEach((imgPrevCon) => {
     imgPrevCon.addEventListener('click', () => {
-        // Hide all text sections
+        // Hide all text sections and header images
         textSecs.forEach((textSec) => {
             textSec.classList.remove('active');
+        });
+
+        headerImgs.forEach((headerImg) => {
+            headerImg.classList.remove('active');
         });
 
         // Show the corresponding text section
         const tabId = imgPrevCon.getAttribute('data-tab');
         const tabContent = document.getElementById(tabId);
         tabContent.classList.add('active');
+
+        // Show the corresponding header image
+        const headerImgId = `header-${tabId}`;
+        const headerImg = document.getElementById(headerImgId);
+        if (headerImg) {
+            headerImg.classList.add('active');
+        }
     });
 });
